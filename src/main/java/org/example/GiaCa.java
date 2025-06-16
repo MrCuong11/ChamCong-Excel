de5c5c62a54e070eeb4390d573ab70c30a654dd4
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-16 15:21:37 +0700

    adding object GiaCa

--- src/main/java/org/example/GiaCa.java
+++ src/main/java/org/example/GiaCa.java
@@ -1,2 +1,22 @@
-package org.example;public class GiaCa {
+package org.example;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GiaCa {
+    Map<String, Double> donGiaCa = new HashMap<>();
+
+    public GiaCa(double gc, double tc, double gc1, double tc1, double wk) {
+        donGiaCa.put("GC", gc);
+        donGiaCa.put("TC", tc);
+        donGiaCa.put("GC1", gc1);
+        donGiaCa.put("TC1", tc1);
+        donGiaCa.put("WK-D", wk);
+        donGiaCa.put("WK-N", wk);
+    }
+
+    public double getGia(String ca) {
+        return donGiaCa.getOrDefault(ca, 0.0);
+    }
 }
